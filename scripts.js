dfb63c1c4654725422a7250b80dbb89732009761
Edvaldo-jb-Junior <juniorb2325@gmail.com>
2025-01-25 09:20:41 -0300

    atualização-animação-scroll

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
+
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach( (entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show")
+        } else {
+            entry.target.classList.remove("show")
+        }
+    })
+})
+
+const elements = document.querySelectorAll(".hidden")
+
+elements.forEach((element) => myObserver.observe(element));
+
+
 const result = document.querySelector(".resultes")
 const humanScore = document.querySelector(".human-score")
 const machineScore = document.querySelector(".machine-score")
